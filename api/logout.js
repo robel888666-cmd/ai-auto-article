82e2ee7a77c87dd4b4695837979031124db126f3
robel888666-cmd <robel888666@gmail.com>
2026-09-14 06:02:13 +0600

    Create logout.js

---
+++ api/logout.js
@@ -0,0 +1,16 @@
+export default async function handler(req, res) {
+
+  res.setHeader(
+    "Set-Cookie",
+
+    "admin_token=; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=0"
+  );
+
+
+  return res.status(200).json({
+
+    success: true
+
+  });
+
+}
